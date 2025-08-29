de8a0e1a6bca36bc60e5f8d5bcd5a6a8c92a01cf
Linn Erle <linnerlek@gmail.com>
2025-08-29 18:11:31 -0400

    pages initial layout before added engines and apps

---
+++ README.md
@@ -0,0 +1,2 @@
+# Formal Visualizer
+This is a combined visualizer for the two engines RAP and Lambda. In the future I plan on building in Datalog and SKI.
---
+++ app.py
@@ -0,0 +1,51 @@
+import dash
+from dash import Dash, html, dcc, Input, Output
+
+app = Dash(__name__, use_pages=True)
+
+page_options = [
+    {"label": p["name"], "value": p["relative_path"]}
+    for p in sorted(dash.page_registry.values(), key=lambda x: x["name"])
+]
+
+app.layout = html.Div([
+    dcc.Location(id="url"),
+
+    html.Div(className="header", children=[
+        html.H1(
+            dcc.Dropdown(
+                id="page-dropdown",
+                options=page_options,
+                clearable=False,
+                value="/",
+                className="dropdown-header"
+            )
+        )
+    ]),
+
+    dash.page_container
+])
+
+
+
+
+@app.callback(
+    Output("url", "pathname"),
+    Output("page-dropdown", "value"),
+    Input("page-dropdown", "value"),
+    Input("url", "pathname"),
+)
+def sync_dropdown(drop_value, pathname):
+    ctx = dash.callback_context
+
+    if ctx.triggered_id == "url":
+        return pathname, pathname
+
+    if ctx.triggered_id == "page-dropdown":
+        return drop_value, drop_value
+
+    return pathname, pathname
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
