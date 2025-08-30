3e98f7267599353e3446c5c30709bd96c1858370
Linn Erle <linnerlek@gmail.com>
2025-08-30 13:26:57 -0400

    updated to use local gifs

--- pages/home.py
+++ pages/home.py
@@ -18,7 +18,7 @@ layout = html.Div([
             html.Div([
                 html.H3("Lambda Calculus Visualizer", className="tool-title"),
                 html.Img(
-                    src="https://private-user-images.githubusercontent.com/89393367/436251850-520e0a27-794d-48b9-acc1-46f064a1a1ae.gif?jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NTY1NzI2MzksIm5iZiI6MTc1NjU3MjMzOSwicGF0aCI6Ii84OTM5MzM2Ny80MzYyNTE4NTAtNTIwZTBhMjctNzk0ZC00OGI5LWFjYzEtNDZmMDY0YTFhMWFlLmdpZj9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNTA4MzAlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjUwODMwVDE2NDUzOVomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWJmMWRlYzI2ZjUzNWRiYzJhNjU4NzgzMDc4ODliNTlmMjViMjA2MmY5OGFhNzc2MWE3NGI4Y2RjMDE3MTc0MWMmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0In0.0NBPrhFc86rmyBdyuAOvLhj3EiU7PQ5QNxHSwch4B24",
+                    src="LambdaDemo.gif",
                     alt="Lambda Calculus Visualizer demo",
                     className="tool-gif"
                 ),
@@ -32,7 +32,7 @@ layout = html.Div([
                 html.H3("Relational Algebra Visualizer",
                         className="tool-title"),
                 html.Img(
-                    src="https://private-user-images.githubusercontent.com/89393367/425778753-9aeb6165-99fc-4c91-be07-0d9ef0d2ad9a.gif?jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NTY1NzI2NjUsIm5iZiI6MTc1NjU3MjM2NSwicGF0aCI6Ii84OTM5MzM2Ny80MjU3Nzg3NTMtOWFlYjYxNjUtOTlmYy00YzkxLWJlMDctMGQ5ZWYwZDJhZDlhLmdpZj9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNTA4MzAlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjUwODMwVDE2NDYwNVomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTE5ZjNkMjlkZTQwYzE0ZDg1ZjA0NjM5MWQ5YmRlNDI5NzI4ZWFkMTFkNTExMjE3MjZiZGFhN2YzY2NhYTQzM2MmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0In0.XT2XHYRAt3zL-fKSBzOgyuuJfyYOcWX-tQMusAv622Y",
+                    src="RADemo.gif",
                     alt="Relational Algebra Visualizer demo",
                     className="tool-gif"
                 ),
